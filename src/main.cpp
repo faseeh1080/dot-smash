bacfd03ba115b29a8254a8e678e333a71e1d3a48
faseeh-official <faseeh1080@outlook.com>
2024-11-30 07:41:13 +0000

    Create Dot class and random number generator.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <random>
+
+int generateRandomNumber(int min, int max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(min, max);
+    return distrib(gen);
+}
+
+class Dot {
+public:
+    sf::CircleShape dot;
+
+    Dot() : dot(50) {
+        dot.setFillColor(sf::Color::Green);
+        dot.setPosition(375, 275);
+    }
+};
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML Window", sf::Style::Close);
 
-	// Game Objects
-	sf::CircleShape dot(50);
-	dot.setFillColor(sf::Color::Green);
-    dot.setPosition(375, 275);
+    Dot theDot;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -18,8 +33,8 @@ int main() {
 
         window.clear();
 
-        window.draw(dot);
-		
+        window.draw(theDot.dot);
+
         window.display();
     }
 
